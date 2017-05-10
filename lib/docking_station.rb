5cfe9fa198d2de7fc88bfe78fa9b61967b5c1e89
charliefea <charlotte.feather@yahoo.com>
2017-05-10 15:21:04 +0100

    Completes challenge 11 to dock bike & return docked bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
 require './lib/bike.rb'
 
 class DockingStation
+attr_reader :bike
+
   def release_bike
     Bike.new
+  end
+
+  def docks_bike (bikeinstance)
+    @bike = bikeinstance
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,4 +7,18 @@ describe DockingStation do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to :docks_bike }
+
+  it "docks bike" do
+    bike = subject.release_bike
+    docked_bike = subject.docks_bike(bike)
+    expect(docked_bike).to eq bike
+  end
+
+  it "returns docked bike" do
+    bike = Bike.new
+    subject.docks_bike(bike)
+    expect(subject.bike).to eq bike
+  end
 end
