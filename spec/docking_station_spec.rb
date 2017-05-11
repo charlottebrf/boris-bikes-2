da0d2fcc47ccb7a3423ed9b0f01dc4c6de34793f
RSijelmass <R.Sijelmass@gmail.com>
2017-05-11 16:40:32 +0100

    Adds method stubs to docking station spec, refactors bike doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,26 +2,24 @@ require 'docking_station'
 
 describe DockingStation do
   subject(:station){ DockingStation.new }
-
+  let(:bike) { double :bike }
   it { is_expected.to respond_to :release_bike }
 
   it "releases working bikes" do
-    bike = double(:bike)
+    allow(bike).to receive(:working?).and_return(true)
     station.docks_bike(bike)
-    bike = station.release_bike
-    expect(bike).to be_working
+    released_bike = station.release_bike
+    expect(released_bike).to be_working
   end
 
   it { is_expected.to respond_to :docks_bike }
 
   it "docks bike" do
-    bike = double(:bike)
     docked_bike = station.docks_bike(bike)
     expect(docked_bike).to eq [bike]
   end
 
   it "returns docked bike" do
-    bike = double(:bike)
     station.docks_bike(bike)
     expect(station.bikes).to eq [bike]
   end
@@ -31,15 +29,15 @@ describe DockingStation do
   end
 
   it "releases same bike as docked" do
-    bike = double(:bike)
+    allow(bike).to receive(:working?).and_return(true)
     station.docks_bike(bike)
     expect(station.release_bike).to eq bike
   end
 
   it "does not release a bike if there are no bikes docked" do
-    DockingStation::DEFAULT_CAPACITY.times {station.docks_bike double(:bike)}
-    bike21 = double(:bike)
-    expect { station.docks_bike(bike21)}.to raise_error('There are no spaces left')
+    DockingStation::DEFAULT_CAPACITY.times {station.docks_bike bike}
+
+    expect { station.docks_bike(bike)}.to raise_error('There are no spaces left')
   end
 
   it "will give default capacity if no parameter is given, else capacity = parameter" do
@@ -50,13 +48,16 @@ describe DockingStation do
   end
 
   it "only releases a bike if it's working" do
-    bike_working = double(:bike)
-    bike_broken = double(:bike)
-    bike_broken.report_broken
+    allow(bike).to receive(:report_broken).and_return(bike)
 
+    bike_working = bike
+    allow(bike_working).to receive(:working?).and_return(true)
     station.docks_bike(bike_working)
     expect(station.release_bike).to eq bike_working
 
+    bike_broken = bike
+    bike_broken.report_broken
+    allow(bike_broken).to receive(:working?).and_return(false)
     station.docks_bike(bike_broken)
     expect {station.release_bike }.to raise_error("Only broken bikes")
   end
